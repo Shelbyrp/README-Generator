4692acdab384e268cc381920ec7ed57b0ea57408
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-05-01 14:39:07 +0800

    Moved index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+let generateMarkdown = require("./utils/generateMarkdown.js")
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
