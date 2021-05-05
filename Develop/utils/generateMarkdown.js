3f62f5697e558e171d7c01a1dfcd6b70fc95c179
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-05-05 20:26:34 +0800

    Tided up the generateMarkDown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,26 +8,17 @@ var licenses = {
 
 let licenseList = () => Object.keys(licenses);
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   return encodeURI(`https://img.shields.io/badge/License-${license}-yellow.svg`);
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   return encodeURI(licenses[license]);
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   return `[![License: ${license}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`;
-  // [![License: ${project.license}](https://img.shields.io/badge/License-${project.license}-yellow.svg)](https://opensource.org/licenses/${project.license})
 }
-
-// TODO: Create a function to generate markdown for README
 
 function generateMarkdown(project) {
   return `
